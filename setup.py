4cbd9431735496770322f11c443f54d3537364f3
Jivan Amara <Development@JivanAmara.net>
2016-03-25 18:13:56 -0700

    Fixed setup.py bug os.path(__file__) -> os.path.abspath(__file__). Supplied author/email/url meta-data. Bumped version to 0.0.1.

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 import os
 
-package_path = os.path.normpath(os.path.join(os.path(__file__), os.path.pardir))
+package_path = os.path.normpath(os.path.join(os.path.abspath(__file__), os.path.pardir))
 os.chdir(package_path)
 
 with open('README.rst') as rm:
     README = rm.read()
 
 setup(
+    author="Jivan Amara",
+    author_email='Development@JivanAmara.net',
+    url='https://github.com/JivanAmara/confirmed-email',
     name='confirmed-email',
-    version='0.0.0',
+    version='0.0.1',
     packages=['confirmed_email'],
     description='Provides an email sender that automatically confirms addresses.',
     long_description=README,
