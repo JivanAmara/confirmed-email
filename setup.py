69416ae48f5d6c34e1ced8878621f9c64accfcdd
Jivan Amara <jivan@lji.org>
2016-03-31 15:35:02 -0700

    Updated MANIFEST.in and setup's package_data to include templates. Bumped version to 0.0.2.

--- setup.py
+++ setup.py
@@ -7,13 +7,20 @@ os.chdir(package_path)
 with open('README.rst') as rm:
     README = rm.read()
 
+confirmed_email_data = [
+    'templates/confirmed_email/address_confirmed.html',
+    'templates/confirmed_email/confirmation_email.txt',
+    'templates/confirmed_email/confirmation_required.html'
+]
+
 setup(
     author="Jivan Amara",
     author_email='Development@JivanAmara.net',
     url='https://github.com/JivanAmara/confirmed-email',
     name='confirmed-email',
-    version='0.0.1',
+    version='0.0.2',
     packages=['confirmed_email'],
+    package_data={'confirmed_email': confirmed_email_data},
     description='Provides an email sender that automatically confirms addresses.',
     long_description=README,
 )
